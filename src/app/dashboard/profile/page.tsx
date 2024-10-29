eb4873b291d1c90aeff1cb63577e3637cab654cb
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2024-10-29 20:10:24 +0100

    Disabled some html

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -6,19 +6,18 @@ import { getDeviceType } from "@/lib/utils";
 // import { Section } from '@/app/ui/layout';
 // import { BookingTable } from "./booking-table";
 import clsx from "clsx";
-import { getSession } from '@auth0/nextjs-auth0';
 
 export default async function Profile() {
   const {isMobile} = getDeviceType(); 
-  const { user } = await getSession();
+
 
   return (
     <>
     {/* {isMobile && <FixedHeader label={user.name} />} */}
       <div className={clsx("flex w-full flex-col", isMobile && "mt-14")}>
         <div className="flex flex-col items-center justify-center bg-primary p-6 max-lg:pb-16 lg:p-12 lg:rounded-4xl">
-          {user && 
-          <><div className="mb-3">
+          
+          {/* <><div className="mb-3">
               <figure className="relative h-32 w-32 overflow-hidden rounded-full border border-gray-100 bg-white">
                 <img
                   src={user.picture}
@@ -31,9 +30,9 @@ export default async function Profile() {
                 <h1 className="text-center font-serif text-3xl font-extrabold text-white">
                   {user.name}
                 </h1>
-                {/* <BookingStatistics bookings={bookings} /> */}
-              </div></>
-          }
+                <BookingStatistics bookings={bookings} />
+              </div></> */}
+          
         </div>
         {/* <Section pt="small">
           <div className="flex flex-col gap-2">
